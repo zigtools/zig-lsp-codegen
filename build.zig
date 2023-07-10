3ec7c7fa9a91d48197703248962621da75a577f5
Techatrix <19954306+Techatrix@users.noreply.github.com>
2023-07-10 23:56:39 +0200

    transition from tres to std.json API

--- build.zig
+++ build.zig
@@ -4,15 +4,12 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const tres_module = b.dependency("tres", .{}).module("tres");
-
     const exe = b.addExecutable(.{
         .name = "lspmm-zig",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    exe.addModule("tres", tres_module);
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -29,10 +26,9 @@ pub fn build(b: *std.build.Builder) void {
     test_step.dependOn(b.getInstallStep());
 
     var tests = b.addTest(.{
-        .root_source_file = .{ .path = "libs/tres/tres.zig" },
+        .root_source_file = .{ .path = "tests/tests.zig" },
         .target = target,
     });
-    tests.addModule("tres", tres_module);
 
     test_step.dependOn(&b.addRunArtifact(tests).step);
 }
