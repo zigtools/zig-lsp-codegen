4635234f936c7048c2c50e08d0a7df2fe6d2b924
Techatrix <techatrix@mailbox.org>
2024-06-28 18:29:12 +0200

    collect code coverage with kcov and upload to Codecov

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![CI](https://github.com/zigtools/zig-lsp-codegen/actions/workflows/main.yml/badge.svg)](https://github.com/zigtools/zig-lsp-codegen/actions)
+[![codecov](https://codecov.io/gh/zigtools/zig-lsp-codegen/graph/badge.svg?token=C3HCN59E4C)](https://codecov.io/gh/zigtools/zig-lsp-codegen)
 [![Documentation](https://badgen.net/badge/icon/Docs?icon=wiki&label)](https://zigtools.github.io/zig-lsp-codegen)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- build.zig
+++ build.zig
@@ -80,4 +80,31 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run all the tests");
     test_step.dependOn(&b.addRunArtifact(lsp_tests).step);
     test_step.dependOn(&b.addRunArtifact(lsp_parser_tests).step);
+
+    // ----------------------------- Code Coverage -----------------------------
+
+    const kcov_merge = std.Build.Step.Run.create(b, "kcov merge coverage");
+    kcov_merge.rename_step_with_output_arg = false;
+    kcov_merge.addArg("kcov");
+    kcov_merge.addArg("--merge");
+    const coverage_output = kcov_merge.addOutputDirectoryArg(".");
+
+    for ([_]*std.Build.Step.Compile{ lsp_tests, lsp_parser_tests }) |test_artifact| {
+        const kcov_collect = std.Build.Step.Run.create(b, "kcov collect coverage");
+        kcov_collect.addArg("kcov");
+        kcov_collect.addArg("--collect-only");
+        kcov_collect.addPrefixedDirectoryArg("--include-pattern=", b.path("."));
+        kcov_merge.addDirectoryArg(kcov_collect.addOutputDirectoryArg(test_artifact.name));
+        kcov_collect.addArtifactArg(test_artifact);
+        kcov_collect.enableTestRunnerMode();
+    }
+
+    const install_coverage = b.addInstallDirectory(.{
+        .source_dir = coverage_output,
+        .install_dir = .{ .custom = "coverage" },
+        .install_subdir = "",
+    });
+
+    const coverage_step = b.step("coverage", "Generate a coverage report with kcov");
+    coverage_step.dependOn(&install_coverage.step);
 }
